1921e7d7f071128a6146f6279ca862f5ce090757
WhatJH <030226jihyun@naver.com>
2026-04-30 21:42:59 +0900

    feat: tailwindcss-animated 패키지 설치 및 설정 추가

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import tailwindcssAnimated from 'tailwindcss-animated';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    tailwindcssAnimated
+  ],
+}
